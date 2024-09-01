36777b005a30b426239ffd50e7171158776bc404
retaj sharaf <retajsharaf@dyn-172-20-101-118.qatar.cmu.local>
2024-09-01 16:28:08 +0300

    solved the issue in the branch

--- src/fib.ts
+++ src/fib.ts
@@ -1,21 +1,13 @@
-module.exports = function fibonacci(n) {
-  if (typeof n !== "number" || n < 0 || !Number.isInteger(n)) {
-    throw new Error("Input must be a non-negative integer.");
-  }
-
-  if (n === 0) {
+// util function that computes the fibonacci numbers
+module.exports = function fibonacci(n: number): number {
+  if (n < 0) {
+    return -1;
+  } else if (n == 0) {
     return 0;
-  } else if (n === 1) {
+  } else if (n == 1) {
     return 1;
   }
 
-  let a = 0, b = 1;
-
-  for (let i = 2; i <= n; i++) {
-    const next = a + b;
-    a = b;
-    b = next;
-  }
-
-  return b;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 };
+
